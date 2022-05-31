e0bd426e1cc9b2b6ee304c438f405a88a4d6bece
emirue <emirue@naver.com>
2022-05-31 17:25:07 +0900

    1일차

---
+++ src/main.js
@@ -0,0 +1,16 @@
+var 안방 = '침대'; // 문자
+var 작은방 = 10; // 숫자
+var 방 = [1,2,3,4,5]; // 배열
+var 주방깨끗해 = false; // 불린(참/거짓)
+
+var aa = '10';
+var bb = 10;
+console.log(aa + bb);
+
+
+/**
+ * 글자를 출력한다.
+ */
+function print() {
+
+}
